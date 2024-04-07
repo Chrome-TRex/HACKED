36d92fb7b5e06f519b65872a50cce4294a168bbd
Chrome-TRex <166300629+Chrome-TRex@users.noreply.github.com>
2024-04-07 17:13:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -778,32 +778,6 @@
          * Game over state.
          */
         gameOver: function () {
-            this.playSound(this.soundFx.HIT);
-            vibrate(200);
-
-            this.stop();
-            this.crashed = true;
-            this.distanceMeter.acheivement = false;
-
-            this.tRex.update(100, Trex.status.CRASHED);
-
-            // Game over panel.
-            if (!this.gameOverPanel) {
-                this.gameOverPanel = new GameOverPanel(this.canvas,
-                    this.spriteDef.TEXT_SPRITE, this.spriteDef.RESTART,
-                    this.dimensions);
-            } else {
-                this.gameOverPanel.draw();
-            }
-
-            // Update the high score.
-            if (this.distanceRan > this.highestScore) {
-                this.highestScore = Math.ceil(this.distanceRan);
-                this.distanceMeter.setHighScore(this.highestScore);
-            }
-
-            // Reset the time clock.
-            this.time = getTimeStamp();
         },
 
         stop: function () {
